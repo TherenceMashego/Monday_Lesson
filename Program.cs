f21a5038f9583c4c74a6ede5defd4d1d49ec1057
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 17:52:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monday_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello World, I am Learning C#!");
+            Console.WriteLine("I love this");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
